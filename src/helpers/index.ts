44ad13f6120f97e99c307df76c8aa528f405d987
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-15 00:05:36 +0200

    helpers util

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 // Authentication helpers to encrypt password or random token
 export const randomizer = () => crypto.randomBytes(128).toString('base64');
-export const authenticationNeedle = (salt: string, password: string) => {
+export const authenticationUtil = (salt: string, password: string) => {
    return crypto.createHmac('sha256', [salt, password].join('/')).update(process.env.SECRET).digest('hex');
 };

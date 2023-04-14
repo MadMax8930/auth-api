11a09f3aa7d760a7f0934b24d51d13d671c8ad0c
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-14 23:16:00 +0200

    router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import authentication from '../router/auth';
+import users from '../router/users';
 
 const router = express.Router();
 
 export default (): express.Router => {
-   authentication(router)
+   authentication(router);
+   users(router);
+
    return router;
-}
+};

40062dd6a3d7850ab4199f47ad64ba7360918d5f
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-14 22:05:38 +0200

    login controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,6 +2,43 @@ import express from 'express';
 import { createUser, getUserByEmail } from '../db/users';
 import { authenticationNeedle, randomizer } from '../helpers';
 
+export const login = async (req: express.Request, res: express.Response) => {
+   try {
+     const { email, password } = req.body;
+
+     if (!email || !password) {
+      return res.sendStatus(400);
+     }
+
+     const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+     if (!user) {
+      return res.sendStatus(400);
+     }
+
+     // Authenticate user without knowing his password (hash comparison)
+     const expectedHash = authenticationNeedle(user.authentication.salt, password);
+
+     if (user.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+     }
+
+     // Update user session token
+     const salt = randomizer();
+     user.authentication.sessionToken = authenticationNeedle(salt, user._id.toString());
+
+     await user.save();
+
+     // Set the cookie
+     res.cookie('MAX-AUTH-COOKIE', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+
+     return res.status(200).json(user).end();
+   } catch (error) {
+     console.log(error);
+     return res.sendStatus(400);
+   }
+}
+
 export const register = async (req: express.Request, res: express.Response) => {
    try {
      const { email, password, username } = req.body;  // extract data that we expect from our body

880fd3a07bf458bd03aeed00848a2ef1248b9e91
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-04-14 23:18:14 +0200

    helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,4 +5,4 @@ require("dotenv").config();
 export const randomizer = () => crypto.randomBytes(128).toString('base64');
 export const authenticationNeedle = (salt: string, password: string) => {
    return crypto.createHmac('sha256', [salt, password].join('/')).update(process.env.SECRET).digest('hex');
-}
+};
